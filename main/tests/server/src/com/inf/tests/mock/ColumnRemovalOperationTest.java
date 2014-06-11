c1d5cfa71dcc50cf6739f355b7556b8eaa89eeaf
lguan2 <guanlin152@hotmail.com>
2014-06-10 19:00:29 -0700

    add testWrite

--- main/tests/server/src/com/inf/tests/mock/ColumnRemovalOperationTest.java
+++ main/tests/server/src/com/inf/tests/mock/ColumnRemovalOperationTest.java
@@ -2,21 +2,30 @@ package com.inf.tests.mock;
 
 import static org.mockito.Mockito.*;
 
+import java.util.Properties;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONWriter;
 import org.testng.annotations.Test;
 
 import com.google.refine.model.Project;
 import com.google.refine.operations.column.ColumnRemovalOperation;
 
-
+/**
+ * @author Lin Guan
+ *
+ */
 
 public class ColumnRemovalOperationTest {
     
     ColumnRemovalOperation cRemoval;
     String name = "newname";
     JSONObject jsonObject;
+    JSONWriter jsonWriter;
     Project mockedProj; 
+    Properties mockedProper;
+    
     
     
     @Test
@@ -31,6 +40,23 @@ public class ColumnRemovalOperationTest {
         
         //verification
         verify(jsonObject, times(1)).getString(anyString());
+        
+    }
+    
+    @Test
+    public void testWrite() throws JSONException{
+        
+        //given
+        jsonWriter = mock(JSONWriter.class);
+        mockedProper = mock(Properties.class);
+        
+        cRemoval.write(jsonWriter, mockedProper);
+        
+        //verification
+        verify(jsonWriter).key("columnName");
+        
+        
+        
         
         
         
